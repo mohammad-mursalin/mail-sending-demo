1e75796b3e749231805e67fe72496ea748e383bd
mohammad-mursalin <mursalinlamon@gmail.com>
2024-12-08 22:08:22 +0600

    initial commit

---
+++ src/test/java/com/mursalin/MailSenderDemo/MailSenderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mursalin.MailSenderDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
