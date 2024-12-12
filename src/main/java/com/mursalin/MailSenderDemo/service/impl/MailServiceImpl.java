6a9c2374e28079be789ddac6f5e87533f367f5f2
mohammad-mursalin <mursalinlamon@gmail.com>
2024-12-12 23:34:00 +0600

    adding new thread to the mail sender

--- src/main/java/com/mursalin/MailSenderDemo/service/impl/MailServiceImpl.java
+++ src/main/java/com/mursalin/MailSenderDemo/service/impl/MailServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,6 +27,7 @@ public class MailServiceImpl implements MailService {
 
 
     @Override
+    @Async
     public void sendSimpleMail(String name, String emailTo, String token) {
 
         try {
--- src/main/java/com/mursalin/MailSenderDemo/service/impl/UserServiceImpl.java
+++ src/main/java/com/mursalin/MailSenderDemo/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import com.mursalin.MailSenderDemo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
